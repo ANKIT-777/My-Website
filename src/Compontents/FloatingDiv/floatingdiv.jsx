7047cece56ab2bcfd613bdbdb0ceb256b4bdbbe3
Ankit Sharma <an2ki4t5s@gmail.com>
2023-04-06 00:39:53 +0530

    installed my image to the content area
    made some cool background (blur)
    
    added some small snippets to make it look attaractive
    
    including the emoji

---
+++ src/Compontents/FloatingDiv/floatingdiv.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './floatingdiv.css'
+
+const FloatingDiv = ({image ,txt1,txt2}) => {
+  return (
+    <div className="floatingdiv">
+        <img src ={image} alt="" />
+        <span className="lt">
+            {txt1} 
+            <br/>
+            {txt2}
+        </span>
+    </div>
+  )
+}
+
+export default FloatingDiv 
--- src/Compontents/Intro.jsx
+++ src/Compontents/Intro.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import './Intro.css'  
 import Github from '../img/github.png';
 import LinkedIn from '../img/linkedin.png';
-import Instagram from '../img/instagram.png'; 
+import Instagram from '../img/instagram.png';
+import Vector1 from '../img/Vector1.png'
+import Vector2 from '../img/Vector2.png'
+import boy from '../img/ankit-1.png'
+import thumbup from '../img/thumbup.png'
+import Crown from '../img/crown.png'
+import glassesimoji from '../img/glassesimoji.png'
+import FloatingDiv from './FloatingDiv/floatingdiv';
+
+
+  
+
 const Intro = () => {
   return (
     <div className="intro">
@@ -29,16 +40,30 @@ const Intro = () => {
          <a href="https://www.instagram.com/axshvlogs2.0/" target="_blank">
          <img src= {Instagram} alt="" />
          </a>
-  
-          
-
         </div> 
 
         </div>
         <div className="i-right">
-          i am right side 
-        </div>
-        
+          <img src={Vector1} alt="" />
+          <img src={Vector2} alt="" />
+          <img src={boy} alt=""/>
+          <img src={glassesimoji} alt="" />
+          <div style={{top:'-4%',left:'68%'}}>
+            <FloatingDiv image = {Crown} txt1='Data' txt2='Scientist'/>
+          </div>
+          <div style={{top: '18rem', left: '0rem'}}>
+            <FloatingDiv image={thumbup} txt1 = 'Competitive' txt2 = 'Coding' />
+          </div>
+          <div className='blur' style={{bacground : "rgb (238 210 255"}}></div>
+          <div className='blue' 
+          style={{
+            bacground : '#C1F5FF',
+            top: '17rem',
+            width: '21rem',
+            height: '11rem',
+            left : '-9rem'
+            }}></div>
+        </div>     
     </div>
   )
 }
